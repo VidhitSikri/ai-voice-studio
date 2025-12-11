1c056867a9c33a499a80186e48535e8f01b4810f
VidhitSikri <sikrividhit@gmail.com>
2025-12-12 02:48:35 +0530

    added polar config and plugin

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -2,7 +2,16 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
+
+import { Polar } from "@polar-sh/sdk";
+
+import { checkout, polar, portal, webhooks } from "@polar-sh/better-auth";
+import { env } from "~/env";
+
+const polarClient = new Polar({
+    accessToken: env.POLAR_ACCESS_TOKEN,
+    server: "sandbox",
+})
 
 
 const prisma = new PrismaClient();
@@ -13,4 +22,69 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+
+
+    plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "4462c236-a526-42da-ad41-e6a37ad899cd",
+              slug: "small",
+            },
+            {
+              productId: "5f5784ed-8253-46b8-9f76-28cf09389753",
+              slug: "medium",
+            },
+            {
+              productId: "b83519d1-0df4-4477-923c-e9bd5b51c88b",
+              slug: "large",
+            },
+          ],
+          successUrl: "/dashboard",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        webhooks({
+          secret: env.POLAR_WEBHOOK_SECRET,
+          onOrderPaid: async (order) => {
+            const externalCustomerId = order.data.customer.externalId;
+
+            if (!externalCustomerId) {
+              console.error("No external customer ID found.");
+              throw new Error("No external customer id found.");
+            }
+
+            const productId = order.data.productId;
+
+            let creditsToAdd = 0;
+
+            switch (productId) {
+              case "4462c236-a526-42da-ad41-e6a37ad899cd":
+                creditsToAdd = 50;
+                break;
+              case "5f5784ed-8253-46b8-9f76-28cf09389753":
+                creditsToAdd = 200;
+                break;
+              case "b83519d1-0df4-4477-923c-e9bd5b51c88b":
+                creditsToAdd = 400;
+                break;
+            }
+
+            await db.user.update({
+              where: { id: externalCustomerId },
+              data: {
+                credits: {
+                  increment: creditsToAdd,
+                },
+              },
+            });
+          },
+        }),
+      ],
+    }),
+  ],
 });
